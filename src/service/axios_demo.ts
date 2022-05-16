c9ce0964124dea6c16dea49f897fbac2ce0d2209
‘guoyanyan’ <guoyanyan@odianyun.com>
2022-05-16 19:41:10 +0800

    feat(request.ts): 封装axios

--- src/service/axios_demo.ts
+++ src/service/axios_demo.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+// import axios from 'axios'
 // axios
 //   .get('http://20', {
 //     params: {
@@ -18,26 +18,26 @@ import axios from 'axios'
 //     console.log(res)
 //   })
 // axios的配置
-axios.defaults.baseURL = 'http://httpbin.org'
-axios.defaults.timeout = 1000
-axios.all([axios.get('http'), axios.post('http')]).then((res) => {
-  console.log(res[0])
-  console.log(res[1])
-})
-const service = axios.create({})
-axios.interceptors.request.use(
-  (config) => {
-    return config
-  },
-  (err) => {
-    return err
-  }
-)
-axios.interceptors.response.use(
-  (res) => {
-    return res.data
-  },
-  (err) => {
-    return err
-  }
-)
+// axios.defaults.baseURL = 'http://httpbin.org'
+// axios.defaults.timeout = 1000
+// axios.all([axios.get('http'), axios.post('http')]).then((res) => {
+//   console.log(res[0])
+//   console.log(res[1])
+// })
+// const service = axios.create({})
+// axios.interceptors.request.use(
+//   (config) => {
+//     return config
+//   },
+//   (err) => {
+//     return err
+//   }
+// )
+// axios.interceptors.response.use(
+//   (res) => {
+//     return res.data
+//   },
+//   (err) => {
+//     return err
+//   }
+// )
